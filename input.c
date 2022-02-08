35a192e09fa1ecccc78639587d2c94656310aafd
Szymon Bęczkowski <szymon.beczkowski@me.com>
2022-02-08 10:34:18 +0100

    rename clear trip button

--- input.c
+++ input.c
@@ -5,7 +5,7 @@
 button button_pressed(void)
 {
     if (GpioDataRegs.GPBDAT.bit.GPIO44 == 1) { return BtnOff; }; // Off
-    if (GpioDataRegs.GPBDAT.bit.GPIO45 == 1) { return BtnReset; }; // Reset
+    if (GpioDataRegs.GPBDAT.bit.GPIO45 == 1) { return BtnClrTrip; }; // Clear trip
     if (GpioDataRegs.GPBDAT.bit.GPIO46 == 1) { return BtnOn; }; // On
     if (GpioDataRegs.GPBDAT.bit.GPIO42 == 1) { return BtnZero; }; // Reference zero
     if (GpioDataRegs.GPBDAT.bit.GPIO39 == 1) { return BtnDecr; }; // Reference decrease
--- input.h
+++ input.h
@@ -2,7 +2,7 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-typedef enum button {BtnNothing, BtnOff, BtnReset, BtnOn, BtnDecr, BtnZero, BtnIncr} button;
+typedef enum button {BtnNothing, BtnOff, BtnClrTrip, BtnOn, BtnDecr, BtnZero, BtnIncr} button;
 
 button button_pressed(void);
 
