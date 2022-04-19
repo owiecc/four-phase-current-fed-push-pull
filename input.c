24f31916b09256776b916df74287d3857c3701bd
Szymon Bęczkowski <szymon.beczkowski@me.com>
2022-04-19 11:52:45 +0200

    inverted logic for button inputs

--- input.c
+++ input.c
@@ -4,11 +4,11 @@
 
 button button_pressed(void)
 {
-    if (GpioDataRegs.GPBDAT.bit.GPIO44 == 1) { return BtnOff; }; // Off
-    if (GpioDataRegs.GPBDAT.bit.GPIO45 == 1) { return BtnClrTrip; }; // Clear trip
-    if (GpioDataRegs.GPBDAT.bit.GPIO46 == 1) { return BtnOn; }; // On
-    if (GpioDataRegs.GPBDAT.bit.GPIO42 == 1) { return BtnZero; }; // Reference zero
-    if (GpioDataRegs.GPBDAT.bit.GPIO39 == 1) { return BtnDecr; }; // Reference decrease
-    if (GpioDataRegs.GPBDAT.bit.GPIO43 == 1) { return BtnIncr; }; // Reference increase
+    if (GpioDataRegs.GPBDAT.bit.GPIO44 == 0) { return BtnOff; }; // Off
+    if (GpioDataRegs.GPBDAT.bit.GPIO45 == 0) { return BtnClrTrip; }; // Clear trip
+    if (GpioDataRegs.GPBDAT.bit.GPIO46 == 0) { return BtnOn; }; // On
+    if (GpioDataRegs.GPBDAT.bit.GPIO42 == 0) { return BtnZero; }; // Reference zero
+    if (GpioDataRegs.GPBDAT.bit.GPIO39 == 0) { return BtnDecr; }; // Reference decrease
+    if (GpioDataRegs.GPBDAT.bit.GPIO43 == 0) { return BtnIncr; }; // Reference increase
     return BtnNothing;
 }
