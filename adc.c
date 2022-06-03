a01a4a3f05e34bdcdb4482eec0f61c77e51fa52a
Szymon Bęczkowski <szymon.beczkowski@me.com>
2022-06-03 15:55:41 +0200

    fix ADC connection

--- adc.c
+++ adc.c
@@ -3,9 +3,9 @@
 #include "f28x_project.h"
 
 struct ADCCalibration ADCCal = {
-    .coeffACD0 = (struct ADCScaling) {.gain = (6*665e3+10e3)/10e3*3.3/4095, .offset = 0}, // (6×665k, 10k) voltage divider
+    .coeffACD0 = (struct ADCScaling) {.gain = (9*665e3+10e3/2+10e3)/10e3*3.3/4095, .offset = 0}, // (9×665k + 10k/2, 10k) voltage divider
     .coeffACD1 = (struct ADCScaling) {.gain = (6*665e3+10e3)/10e3*3.3/4095, .offset = 0}, // (6×665k, 10k) voltage divider
-    .coeffACD2 = (struct ADCScaling) {.gain = (9*665e3+10e3/2+10e3)/10e3*3.3/4095, .offset = 0}, // (9×665k + 10k/2, 10k) voltage divider
+    .coeffACD2 = (struct ADCScaling) {.gain = (6*665e3+10e3)/10e3*3.3/4095, .offset = 0}, // (6×665k, 10k) voltage divider
     .coeffACD3 = (struct ADCScaling) {.gain = 6.0/0.625*(3.3e3+6.8e3)/6.8e3*3.3/4095, .offset = (unsigned int) 2.5*6.8e3/(3.3e3+6.8e3)*4095/3.3} // LEM6-NP + (3.3k, 6.8k) voltage divider
 };
 
@@ -26,9 +26,9 @@ struct ADCResult readADC(void)
 inline struct ADCResult scaleADCs(void)
 {
     struct ADCResult adcOut;
-    adcOut.Vin = scaleADC(AdcaResultRegs.ADCRESULT0, ADCCal.coeffACD0);
-    adcOut.Vout = scaleADC(AdcaResultRegs.ADCRESULT1, ADCCal.coeffACD1);
-    adcOut.Vclamp = scaleADC(AdcaResultRegs.ADCRESULT2, ADCCal.coeffACD2);
+    adcOut.Vclamp = scaleADC(AdcaResultRegs.ADCRESULT0, ADCCal.coeffACD0);
+    adcOut.Vin = scaleADC(AdcaResultRegs.ADCRESULT1, ADCCal.coeffACD1);
+    adcOut.Vout = scaleADC(AdcaResultRegs.ADCRESULT2, ADCCal.coeffACD2);
     adcOut.Iout = scaleADC(AdcaResultRegs.ADCRESULT3, ADCCal.coeffACD3);
 
     return adcOut;
@@ -58,15 +58,15 @@ void initADCSOC(void)
 {
     // Select the channels to convert and the end of conversion flag
     EALLOW;
-    AdcaRegs.ADCSOC0CTL.bit.CHSEL = 0;     // SOC0 will convert pin A0 = Vin
+    AdcaRegs.ADCSOC0CTL.bit.CHSEL = 0;     // SOC0 will convert pin A0 = Vclamp
     AdcaRegs.ADCSOC0CTL.bit.ACQPS = 9;     // Sample window is 10 SYSCLK cycles
     AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = 5;   // Trigger on ePWM1 SOCA
 
-    AdcaRegs.ADCSOC1CTL.bit.CHSEL = 1;     // SOC1 will convert pin A1 = Vout
+    AdcaRegs.ADCSOC1CTL.bit.CHSEL = 1;     // SOC1 will convert pin A1 = Vin
     AdcaRegs.ADCSOC1CTL.bit.ACQPS = 9;     // Sample window is 10 SYSCLK cycles
     AdcaRegs.ADCSOC1CTL.bit.TRIGSEL = 5;   // Trigger on ePWM1 SOCA
 
-    AdcaRegs.ADCSOC2CTL.bit.CHSEL = 2;     // SOC2 will convert pin A2 = Vclamp
+    AdcaRegs.ADCSOC2CTL.bit.CHSEL = 2;     // SOC2 will convert pin A2 = Vout
     AdcaRegs.ADCSOC2CTL.bit.ACQPS = 9;     // Sample window is 10 SYSCLK cycles
     AdcaRegs.ADCSOC2CTL.bit.TRIGSEL = 5;   // Trigger on ePWM1 SOCA
 
