d0b60d11bb7b7aaf08fbe13b33f89b8064f55775
Szymon Bęczkowski <szymon.beczkowski@me.com>
2022-06-17 13:02:17 +0200

    set 2A referernce

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ void main(void)
         {
             relayOn();
             initPIConttrollers();
-            setControllerIoutRef(5.0);
+            setControllerIoutRef(2.0);
             setControllerVclampRef(0.0);
             enablePWM();
             converter_state = StateOn;
